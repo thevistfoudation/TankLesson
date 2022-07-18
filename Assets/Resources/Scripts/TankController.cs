23724137515ae475ecc9413839e596377fbc3ccd
hieulm <hieulm@zitga.com.vn>
2022-07-18 20:48:52 +0700

    add tank

---
+++ Assets/Resources/Scripts/TankController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    public int speed;
+    public GameObject bodyTank;
+    public GameObject gun;
+    public GameObject bullet;
+    public GameObject transhoot;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, vertical);
+
+        if (direction != Vector3.zero)
+        {
+            this.gameObject.transform.up = direction;
+        }
+
+        bodyTank.transform.position += direction * Time.deltaTime * speed;
+
+        Vector3 gunDirection = new Vector3(
+               Input.mousePosition.x - Screen.width / 2,
+               Input.mousePosition.y - Screen.height / 2
+           );
+
+        gun.transform.up = gunDirection;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, transhoot.transform.position, transhoot.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/bulletController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour
+{
+   public float speed;
+    private float time = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time == 80000)
+        {
+            Destroy(this.gameObject);
+        }
+        time++;
+        this.transform.position += transform.up * Time.deltaTime * speed;
+    }
+}
